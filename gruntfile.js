634ff76e35863dcae629eef1fd2c0712725d31bc
Liuxin <130866602@qq.com>
2016-03-14 18:29:42 +0800

    [ui] components

---
+++ gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        shell: {
+            jekyllBuild: {
+                command: 'jekyll build'
+            }
+        },
+        connect: {
+            server: {
+                options: {
+                    port: 8080,
+                    base: '_site'
+                }
+            }
+        },
+        watch: {
+          livereload: {
+            files: [
+                '_src/_config.yml',
+                '_src/_data/**',
+                '_src/index.html',
+                '_src/_layouts/**',
+                '_src/_posts/**',
+                '_src/_includes/**',
+                '_src/_less/**'
+            ],
+            tasks: ['shell:jekyllBuild'],
+            options: {
+              livereload: true
+            },
+          },
+          less : {
+              files : ['./**/**/*.less'],
+              tasks: ['less','shell:jekyllBuild'],
+              options: {
+                livereload: true
+              },
+          }
+      },
+      less: {
+        development: {
+            options: {
+                paths: ["./src/_less"]
+            },
+            files: [
+                {
+                  "./_src/css/bootstrap.css" : "./_src/_less/bootstrap.less"
+                },
+                {
+                   "./_src/css/doc.css" : "./_src/_less/doc.less"
+                }
+            ]
+        }
+      }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.registerTask('default', ['shell', 'connect', 'watch'])
+};
